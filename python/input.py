8ebbb98d6c2ecffdec70f65c9108272ecb21a379
chelophoto <chelophoto@gmail.com>
2016-11-14 21:30:38 -0500

    Calculator

---
+++ python/input.py
@@ -0,0 +1,11 @@
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+cost = float (raw_input ('How much was your meal?'))
+tip = int(cost) * 0.20
+total = cost + tip
+print('You should tip $' + str(tip))
+print ('Your total cost would be $' + str(total))
