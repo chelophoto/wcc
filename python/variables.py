940b5c5d5d4e1f3ebce5ff65a8e176fcd8e1028f
chelophoto <chelophoto@gmail.com>
2016-11-14 19:34:08 -0500

    Variables Practice

---
+++ python/variables.py
@@ -0,0 +1,6 @@
+first_name = 'Ariel'
+last_name = 'Brewer'
+
+print ('Student first name:' + first_name)
+print('Student last name:' + last_name)
+print('Student name: ' + first_name + ' ' + last_name )
